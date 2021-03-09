d6598aedd1526144f212d524981aae6b4487825d
N-Santiago <norbertosantiago82@gmail.com>
2021-03-08 20:07:52 -0600

    Albums data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do 
+    Album.create(name: Faker::Music.album, artist: Faker::Music.band, format: "Vynil", condition: "Sealed", description: "Repressing", price: rand() * 100)
+end 
