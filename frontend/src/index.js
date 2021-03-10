99818901b3f330eaa4e057ac1af434d4f9042dda
N-Santiago <norbertosantiago82@gmail.com>
2021-03-10 17:30:00 -0600

    created and called init

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,34 @@
-// Make fetch to get data from server
+const BASE_URL = "http://localhost:3000";
+const albumEl = document.getElementById("album-list")
+
+const init = () => {
+    getAlbums();
+};
+  
+const getAlbums = () => {
 fetch("http://localhost:3000/albums")
-.then((res) => res.json())
-.then((data) => console.log(data))
+    .then((res) => res.json())
+    .then((data) => renderAlbums(data));
+}
+
+const renderAlbums = function (albums) {
+    albums.forEach((album) => {
+        albumEl.innerHTML += `
+        <div>
+            <h3>${album.name}<br>
+            by ${album.artist}</h3>
+            <p>Format: ${album.format}<br>
+            Condition: ${album.condition}<br>
+            Description: ${album.description}<br>
+            Price: ${formatPrice(album.price)}</p>
+        </div>
+        `;    
+    });
+};
+
+function formatPrice(price) {
+    return `$${price}`;
+}
+
+// Have to call init in order to get what I'm calling inside of it. 
+init()
