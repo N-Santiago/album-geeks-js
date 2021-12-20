5a031a3b73eff7da8560c83a46a83288140d5e79
N-Santiago <norbertosantiago82@gmail.com>
2021-12-20 06:52:10 -0600

    Changes to Album model

--- backend/app/models/album.rb
+++ backend/app/models/album.rb
@@ -1,8 +1,13 @@
 class Album < ApplicationRecord
     belongs_to :category, optional: true 
+    has_one_attached :image
 
     def category_name
         self.category ? self.category.name : "Not assigned" 
     end 
+
+    def image_url
+        url_for(self.image)
+    end
 
 end
