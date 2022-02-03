baf497e4dc2f690d880fe35fe3fad96117826b74
N-Santiago <norbertosantiago82@gmail.com>
2022-02-02 22:03:44 -0600

    Added font and make some UI changes

--- frontend/src/album.js
+++ frontend/src/album.js
@@ -36,7 +36,7 @@ class Album {
         return `
             <div class="mt-4">
                 <a  href="#"><h3 data-id=${this.id} class="font-medium hover:text-gray-400 album-link">${this.name}<br>
-                by ${this.artist}</h3></a><button data-id=${this.id} type="button" class="delete-btn bg-red-500 hover:bg-red-700 text-white font-medium py-1 px-2 border border-red-700 rounded">Bought</button>
+                by ${this.artist}</h3></a><button data-id=${this.id} type="button" class="delete-btn bg-red-500 hover:bg-red-700 text-white py-1 px-2 border border-red-700 rounded">Bought</button>
             </div>
         `
     } 
