342f12db7abca45758a017319d548ceee8801aec
N-Santiago <norbertosantiago82@gmail.com>
2022-02-09 20:17:59 -0600

    New DB and model validations

--- backend/app/models/album.rb
+++ backend/app/models/album.rb
@@ -1,7 +1,7 @@
 class Album < ApplicationRecord
     belongs_to :category 
     validates :name, :artist, presence: true, length: { in: 3..35, too_long: "%{count} characters is the minimum and maximum allowed" }
-    validates :description, length: { maximum: 35, too_long: "%{count} characters is the minimum and maximum allowed" }
+    validates :description, length: { maximum: 35, too_long: "%{count} characters is the maximum allowed" }
     validates :price, presence: true
 
     def category_name
